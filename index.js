8a56f7444441f2369c800e91192a2cb8c988e52f
AndrewDAntonio <github email address>
2020-03-30 08:13:02 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let thankYous = []
+    for (let i = 0; i < arr.length; i++) {
+        thankYous.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(num) {
+    let i = 0;
+    while(i <= num) {
+        console.log(num - i);
+        i++
+    }
+}
